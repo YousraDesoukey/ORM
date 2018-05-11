3e06ad913dfefb3a6a8a06d037ef5d8245210aeb
YousraDesoukey <yousradesoukey@hotmail.com>
2018-05-11 21:49:56 +0200

    Simple ORM

--- src/maintenance1/Maintenance1.java
+++ src/maintenance1/Maintenance1.java
@@ -10,21 +10,21 @@ public class Maintenance1 {
    public static void main(String[] args) {
    Connection conn = null;
    Statement stmt = null;
-  client d= new client("bank", "root", "", 2, "dsffa", "el sabesafs3", "01411111", false);
+  client d= new client("bank", "root", "", 5, "yousra", "el sabesafs3", "01411111");
   try{
-    d.connect();
-    //d.createTable();
-    d.save();
-   
+//    d.connect();
+//    d.createTable();
+//  d.save();
+//   
     d.connect();
  ArrayList l=d.getBy("client_name","yousra");
  for (int i=0;i<l.size();i++)
  {
-     System.out.println(((client)l.get(i)).getClient_id());
-     System.out.println(((client)l.get(i)).getClient_name());
-     System.out.println(((client)l.get(i)).getBranch_name());
-     System.out.println(((client)l.get(i)).getPhone_number());
-     System.out.println(((client)l.get(i)).getBoolean());
+     System.out.println("Object no: "+ i+1);
+     System.out.println(((client)l.get(i)).getClient_id() + ", " + ((client)l.get(i)).getClient_name() + ", " + ((client)l.get(i)).getBranch_name() + ", " + ((client)l.get(i)).getPhone_number());
+
+     
+
  }
   }
   catch(Exception e)
